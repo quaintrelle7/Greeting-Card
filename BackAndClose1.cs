62020d5fb8c54e143940167da9e7aaafbac429e6
quaintrelle7 <64580890+quaintrelle7@users.noreply.github.com>
2020-09-09 14:11:51 +0530

    Add files via upload

---
+++ BackAndClose1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackAndClose1 : MonoBehaviour
+{
+  public void BackButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void CloseButton()
+        {
+            Application.Quit();
+            Debug.Log("Exit button is pressed");
+            //Debug.Log("klsk");
+        }
+}
